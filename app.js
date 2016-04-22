bce5e36682e07bb3c2d3d938cdf8e04ef82dd16a
jur4ik <jur4ik@gmx.de>
2016-04-22 15:24:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 angular.module('HelloUserApp', [])
           .controller('HelloUserController', function($scope) {
+              $scope.greeting ="";
               $scope.NameChange = function () {
                   $scope.greeting = "Hello " + $scope.name;
               };
